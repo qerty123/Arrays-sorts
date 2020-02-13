3f9ed45799f0e50dc5d62ad0cde4de0d822d2ab1
Stanislav <delphi.troll@mail.ru>
2020-02-13 19:53:52 +0500

    Average time

--- genarray.py
+++ genarray.py
@@ -1,6 +1,6 @@
 import random
 
 file = open('array.txt', 'w')
-for i in range(100):
+for i in range(1000):
     file.write(str(random.randint(-1000, 1000)) + '\n')
 file.close()
--- main.py
+++ main.py
@@ -9,25 +9,35 @@ print('Starting array: ' + str(array))
 
 # Bubble sort
 st = time()
+for i in range(99):
+    sort_bubble(array)
 print('Bubble sort: ' + str(sort_bubble(array)))
-print('Time: ' + "%s seconds" % (time() - st).__round__(4))
+print('Time: ' + "%s seconds" % ((time() - st) / 100).__round__(4))
 
 # Countion sort
 st = time()
+for i in range(99):
+    sort_countion(array)
 print('Countion sort: ' + str(sort_countion(array)))
-print('Time: ' + "%s seconds" % (time() - st).__round__(4))
+print('Time: ' + "%s seconds" % ((time() - st) / 100).__round__(4))
 
 # Simple include sort
 st = time()
+for i in range(99):
+    sort_include(array)
 print('Simple include sort: ' + str(sort_include(array)))
-print('Time: ' + "%s seconds" % (time() - st).__round__(4))
+print('Time: ' + "%s seconds" % ((time() - st) / 100).__round__(4))
 
 # Simple eject sort
 st = time()
+for i in range(99):
+    sort_eject(array)
 print('Simple eject sort: ' + str(sort_eject(array)))
-print('Time: ' + "%s seconds" % (time() - st).__round__(4))
+print('Time: ' + "%s seconds" % ((time() - st) / 100).__round__(4))
 
 # Shell sort
 st = time()
+for i in range(99):
+    sort_shell(array)
 print('Shell sort: ' + str(sort_shell(array)))
-print('Time: ' + "%s seconds" % (time() - st).__round__(4))
+print('Time: ' + "%s seconds" % ((time() - st) / 100).__round__(4))

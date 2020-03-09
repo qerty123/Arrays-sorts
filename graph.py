a3e2c060f9624814f9705e3722c9bb8c190278a2
Stanislav <delphi.troll@mail.ru>
2020-03-09 23:06:11 +0500

    Add progress bar, logs, extra settings

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-colors = ['#FF00FF', '#800080', '#FF0000', '#800000', '#FFFF00', '#808000', '#00FF00', '#008000', '#00FFFF', '#008080', '#0000FF', '#000080']
-#ax = plt.subplot()
+colors = ['#FF00FF', '#800080', '#FF0000', '#800000', '#FFFF00', '#808000', '#00FF00', '#008000', '#00FFFF', '#008080',
+          '#0000FF', '#000080']
 file = open('results.txt', 'r')
 o = 0
 for i in file:
@@ -13,10 +13,11 @@ for i in file:
             pass
     plt.plot(array, linestyle='-', color=colors[o])
     o += 1
-#ax.set_facecolor('#DCDCDC')
+# ax.set_facecolor('#DCDCDC')
 plt.title('Comparison of sorts')
 plt.xlabel('Elements')
 plt.ylabel('Time')
-plt.legend(('Bubble', 'Countion', 'Simple include', 'Simple eject', 'Shell sort', 'Tree sort', 'Quick sort', 'Merge sort', 'Bitwise'))
+plt.legend(('Bubble', 'Countion', 'Simple include', 'Simple eject', 'Shell sort', 'Tree sort', 'Quick sort',
+            'Merge sort', 'Bitwise'))
 plt.grid()
 plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from methods import *
 from time import time
 from random import randint
+from progressbar import progressbar
 
 iterations = 10
+total = 5000
 objects = False
+logs = False
 
 results_bubble = []
-results_countion = []
+results_countion = [0]
 results_sinclude = []
-results_seject = []
+results_seject = [0]
 results_shell = []
 results_tree = []
 results_quicksort = []
@@ -26,70 +29,83 @@ def make_sorts(array):
     st = time()
     for i in range(iterations - 1):
         sort_bubble(array)
-    print('Bubble sort: ' + str(sort_bubble(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Bubble sort: ' + str(sort_bubble(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_bubble.append((time() - st) / iterations)
 
+    """
     # Countion sort
     st = time()
     for i in range(iterations - 1):
         sort_countion(array)
-    print('Countion sort: ' + str(sort_countion(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Countion sort: ' + str(sort_countion(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_countion.append((time() - st) / iterations)
+    """
 
     # Simple include sort
     st = time()
     for i in range(iterations - 1):
         sort_include(array)
-    print('Simple include sort: ' + str(sort_include(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Simple include sort: ' + str(sort_include(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_sinclude.append((time() - st) / iterations)
 
+    """
     # Simple eject sort
     st = time()
     for i in range(iterations - 1):
         sort_eject(array)
-    print('Simple eject sort: ' + str(sort_eject(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Simple eject sort: ' + str(sort_eject(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_seject.append((time() - st) / iterations)
+    """
 
     # Shell sort
     st = time()
     for i in range(iterations - 1):
         sort_shell(array)
-    print('Shell sort: ' + str(sort_shell(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Shell sort: ' + str(sort_shell(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_shell.append((time() - st) / iterations)
 
     # Tree sort
     st = time()
     for i in range(iterations - 1):
         sort_tree(array)
-    print('Tree sort: ' + str(sort_tree(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Tree sort: ' + str(sort_tree(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_tree.append((time() - st) / iterations)
 
     # Quick sort
     st = time()
     for i in range(iterations - 1):
         sort_quick(array)
-    print('Quick sort: ' + str(sort_quick(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Quick sort: ' + str(sort_quick(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_quicksort.append((time() - st) / iterations)
 
     # Merge sort
     for i in range(iterations - 1):
         sort_merge(array)
-    print('Merge sort: ' + str(sort_merge(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Merge sort: ' + str(sort_merge(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_merge.append((time() - st) / iterations)
 
     # Bitwise sort
     for i in range(iterations - 1):
         sort_bitwise(array)
-    print('Bitwise sort: ' + str(sort_bitwise(array)))
-    print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
+    if logs:
+        print('Bitwise sort: ' + str(sort_bitwise(array)))
+        print('Time: ' + "%s seconds" % ((time() - st) / iterations).__round__(4))
     results_bitwise.append((time() - st) / iterations)
 
 
@@ -101,7 +117,15 @@ def make_str(array):
     return string
 
 
-for p in range(100, 20000, 100):
+tt = time()
+if not logs:
+    bar = progressbar.ProgressBar(maxval=total).start()
+    print('Sort in progress: ')
+for p in range(100, total, 100):
+    if logs:
+        print('========== New iteration (' + str(p) + ') ==========')
+    else:
+        bar.update(p)
     array = []
     if objects:
         for o in range(0, p):
@@ -110,6 +134,8 @@ for p in range(100, 20000, 100):
         for o in range(0, p):
             array.append(randint(-10000, 10000))
     make_sorts(array)
+bar.finish()
+print('Total time: ' + str((time() - tt).__round__(4)))
 
 file = open('results.txt', 'w')
 file.write(make_str(results_bubble))
